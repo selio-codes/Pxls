6a2722049d949dbe28d60501702398d1cf4558e2
Mikarific <mikerific2@gmail.com>
2020-08-17 13:44:10 -0600

    Fix even more eslint errors

--- resources/public/pxls.js
+++ resources/public/pxls.js
@@ -3757,7 +3757,7 @@ window.App = (function() {
     if (location.href.includes('auth_invalid=true')) {
       modal.show(modal.buildDom(
         crel('h2', { class: 'modal-title' }, 'Error'),
-        crel('p', { style: 'padding: 0; margin: 0;' }, 'Your authentication email must end with \"@s.stemk12.org\" or \"@stemk12.org\". Please try again.')
+        crel('p', { style: 'padding: 0; margin: 0;' }, 'Your authentication email must end with "@s.stemk12.org" or "@stemk12.org". Please try again.')
       ), { closeExisting: false });
     }
 
