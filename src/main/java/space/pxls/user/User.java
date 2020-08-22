8f80310c39486dc29aaf0ca614520abf5beda3bd
Mikarific <mikerific2@gmail.com>
2020-08-22 03:07:31 -0600

    Donator gradient changes

--- src/main/java/space/pxls/user/User.java
+++ src/main/java/space/pxls/user/User.java
@@ -620,6 +620,11 @@ public class User {
                 && this.chatNameColor == -1;
     }
 
+    public boolean hasDonatorChatNameColor() {
+        return hasPermission("chat.usercolor.donator")
+                && this.chatNameColor == -2;
+    }
+
     public int getChatNameColor() {
         return this.chatNameColor;
     }
@@ -628,6 +633,9 @@ public class User {
         List<String> toReturn = new ArrayList<>();
         if (this.hasRainbowChatNameColor()) {
             toReturn.add("rainbow");
+        }
+        if (this.hasDonatorChatNameColor()) {
+            toReturn.add("donator");
         }
         return toReturn.size() != 0 ? toReturn : null;
     }
